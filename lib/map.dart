d653f0434091d877da1785a04af968b320e1e1c5
atdh <dhungelatul@yahoo.com>
2019-04-24 23:34:46 -0600

    logout and pin works

--- lib/map.dart
+++ lib/map.dart
@@ -79,8 +79,21 @@ class MapPageState extends State<MapPage>{
   Marker _pin = Marker(
     markerId: MarkerId('_pin1'),
     position: LatLng(40.006949, -105.262697)
+    //position: LatLng(40.009448,-105.266813)
+    
     );      
+   Marker _pin1 = Marker(
+    markerId: MarkerId('_pin2'),
+    position: LatLng(40.008708, -105.273021)
+    //position: LatLng(40.009448,-105.266813)
+    );    
 
+    Marker _pin2 = Marker(
+    markerId: MarkerId('_pin3'),
+    position: LatLng(40.009448, -105.266813)
+    //position: LatLng(40.009448,-105.266813)
+    );    
+    
 
 
 
@@ -173,7 +186,7 @@ class MapPageState extends State<MapPage>{
                        _controller.complete(controller);
                        },
                       markers: {
-              _pin,
+              _pin,_pin1,_pin2,
             }
 
 
@@ -359,294 +372,3 @@ class MapPageState extends State<MapPage>{
       
 
 
-//         drawer: new Container(
-//           width: 250.0,
-//           color: Colors.orangeAccent,
-//           child: new ListView(
-//             children: <Widget>[
-//               new DrawerHeader(
-//                 child: new Text('Hello!',
-//                   style: new TextStyle(
-//                     fontSize: 16.0,
-//                     color: const Color(0xFFFFAB40),
-//                     fontWeight: FontWeight.bold,
-//                     fontFamily: "Roboto"
-//                   ),
-//                 ),
-//                 decoration: new BoxDecoration(
-//                   color: Colors.white,
-//                 ),
-//               ),
-//               Container(
-//                 // This align moves the children to the bottom
-//                   child: Align(
-//                       alignment: FractionalOffset.center,
-//                       child: Container(
-//                           child: Column(
-//                             children: <Widget>[
-//                               //Divider(),
-//                               ListTile(
-//                                   leading: Container(
-//                                     child: FlatButton(
-//                                       child: Align(
-//                                         alignment: Alignment.centerLeft,
-//                                         child: Text("Profile",
-//                                           style: TextStyle(
-//                                             fontSize: 16.0,
-//                                             color: Colors.white,
-//                                           ),
-//                                         ),
-//                                       ),
-//                                       onPressed: (){
-//                                         Navigator.of(context).pushNamed('/profile_page');
-//                                       },
-//                                     ),
-//                                   )
-// //                                  Icon(Icons.settings),
-// //                                  title: Text('Settings')),
-//                               ),
-//                               ListTile(
-//                                   leading: Container(
-//                                     child: FlatButton(
-//                                       child: Align(
-//                                         alignment: Alignment.centerLeft,
-//                                         child: Text("Log Out",
-//                                           style: TextStyle(
-//                                             fontSize: 16.0,
-//                                             color: Colors.white,
-//                                           ),
-//                                         ),
-//                                       ),
-//                                       onPressed: (){
-//                                         widget.lougOut(context);
-//                                       },
-//                                     ),
-//                                   )
-// //                                  Icon(Icons.settings),
-// //                                  title: Text('Settings')),
-//                               ),
-
-
-
-
-
-//                             ],
-//                           )
-//                       )
-//                   )
-//               )
-//             ],
-//           ),
-//         ),
-
- 
-            // mainAxisSize: MainAxisSize.min,
-            // children: <Widget>[
-            //   OutlineButton.icon(
-            //     icon: const Icon(Icons.add, size: 18.0),
-            //     label: const Text('Menu', semanticsLabel: 'Menu'),
-            //     onPressed: () => _scaffoldKey.currentState.openDrawer(),
-            //   ),
-            //   OutlineButton.icon(
-            //     icon: const Icon(Icons.add, size: 18.0),
-            //     label: const Text('DISABLED', semanticsLabel: 'DISABLED BUTTON 6'),
-            //     onPressed: null,
-            //   ),
-            // ],
-          // ButtonBar(
-          //   mainAxisSize: MainAxisSize.min,
-          //   children: <Widget>[
-          //     OutlineButton(
-          //       child: const Text('To the Field!', semanticsLabel: 'To the Field!'),
-          //       onPressed: _goToTheField,
-          //     ),
-          //      OutlineButton(
-          //       child: Text('Choose/Create an Activity', semanticsLabel: 'Create Event'),
-          //        onPressed: (){
-          //           Navigator.of(context).pushNamed('/event_create');
-          //         },
-          //     ),
-          //   ],
-          // ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: _goToTheField,
-      //   label: Text('To the Field!'),
-      //   icon: Icon(Icons.place),
-      // ),
-
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: (){
-      //               Navigator.of(context).pushNamed('/event_create');
-      //             },
-      //   label: Text('Choose/Create an Activity'),
-      //   icon: Icon(Icons.place),
-      // ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: () => _scaffoldKey.currentState.openDrawer(),
-      //   label: Text('Menu'),
-      //   icon: Icon(Icons.place),
-      // ),
-      //),
-      // Padding(
-      //       padding: const EdgeInsets.all(25.0),
-      //       child: Align(
-      //         alignment: Alignment.topLeft,
-      //         child: IconButton(
-      //           tooltip: 'Menu',
-      //           color: Colors.orangeAccent,
-      //             iconSize: 32.00,
-      //             icon: Icon(
-      //               Icons.menu
-      //             ),
-      //           onPressed: () => _scaffoldKey.currentState.openDrawer(),
-      //         ),
-      //       ),
-      //     ),
-// //          SizedBox(height: 10.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topCenter,
-//               child: ButtonTheme(
-//                 minWidth: 8.00,
-//                 height: 35.00,
-//                 child: RaisedButton(
-//                   child: const Text('Choose/Create an Activity'),
-//                   textColor: Colors.white,
-//                   color: Colors.orangeAccent,
-//                   elevation: 4.0, //shadow of button
-//                   splashColor: Color(0xFF424242),
-//                   onPressed: (){
-//                     Navigator.of(context).pushNamed('/event_create');
-//                   }
-//                 ),
-//               ),
-//             ),
-//           )
-//            ],
-       
-//        floatingActionButton: new Builder(builder: (context) {
-//          return new FloatingActionButton(
-//            backgroundColor: Colors.transparent,
-//            onPressed: () {
-//              Scaffold.of(context).openDrawer();
-//            },
-//            child: new Icon(Icons.menu),
-//            foregroundColor: Colors.orangeAccent,
-//          );
-//        }),
-
-            
-//           GoogleMap(
-//                 onMapCreated: _onMapCreated,
-//                 initialCameraPosition: CameraPosition(
-//                   bearing: 90.0,
-//                   target: LatLng(40.007565, -105.267298),
-//                   tilt: 20, //talk to group and adjust
-//                   zoom: 15.5
-//                 ),
-//               ),
-//               floatingActionButton: FloatingActionButton.extended(
-//         onPressed: _goToTheLake,
-//         label: Text('To the lake!'),
-//         icon: Icon(Icons.directions_boat),
-//       ),
-// //          SizedBox(height: 30.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topLeft,
-//               child: IconButton(
-//                 tooltip: 'Menu',
-//                 color: Colors.orangeAccent,
-//                   iconSize: 32.00,
-//                   icon: Icon(
-//                     Icons.menu
-//                   ),
-//                 onPressed: () => _scaffoldKey.currentState.openDrawer(),
-//               ),
-//             ),
-//           ),
-// //          SizedBox(height: 10.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topCenter,
-//               child: ButtonTheme(
-//                 minWidth: 8.00,
-//                 height: 35.00,
-//                 child: RaisedButton(
-//                   child: const Text('Choose/Create an Activity'),
-//                   textColor: Colors.white,
-//                   color: Colors.orangeAccent,
-//                   elevation: 4.0, //shadow of button
-//                   splashColor: Color(0xFF424242),
-//                   onPressed: (){
-//                     Navigator.of(context).pushNamed('/event_create');
-//                   }
-//                 ),
-//               ),
-//             ),
-//           )
-//             ],
-//           ),
-//         ),
-
-//       );
-//     Future<void> _goToTheLake() async {
-//     final GoogleMapController controller = await _controller.future;
-//     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-//   }
-//   }
-// }
-
-//  Stack(
-//           children: <Widget>[
-//           GoogleMap(
-//                 onMapCreated: _onMapCreated,
-//                 initialCameraPosition: CameraPosition(
-//                   bearing: 90.0,
-//                   target: LatLng(40.007565, -105.267298),
-//                   tilt: 20, //talk to group and adjust
-//                   zoom: 15.5
-//                 ),
-//               ),
-// //          SizedBox(height: 30.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topLeft,
-//               child: IconButton(
-//                 tooltip: 'Menu',
-//                 color: Colors.orangeAccent,
-//                   iconSize: 32.00,
-//                   icon: Icon(
-//                     Icons.menu
-//                   ),
-//                 onPressed: () => _scaffoldKey.currentState.openDrawer(),
-//               ),
-//             ),
-//           ),
-// //          SizedBox(height: 10.0,),
-//           Padding(
-//             padding: const EdgeInsets.all(25.0),
-//             child: Align(
-//               alignment: Alignment.topCenter,
-//               child: ButtonTheme(
-//                 minWidth: 8.00,
-//                 height: 35.00,
-//                 child: RaisedButton(
-//                   child: const Text('Choose/Create an Activity'),
-//                   textColor: Colors.white,
-//                   color: Colors.orangeAccent,
-//                   elevation: 4.0, //shadow of button
-//                   splashColor: Color(0xFF424242),
-//                   onPressed: (){
-//                     Navigator.of(context).pushNamed('/event_create');
-//                   }
-//                 ),
-//               ),
-//             ),
-//           )
-//             ],
-//           ),
